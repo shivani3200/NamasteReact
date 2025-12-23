936b5e12ed13ca86d61878de769a6e2519b975d2
shivani3200 <shivani13222@gmail.com>
2025-12-23 08:58:16 +0530

    added gitignore, app.js, index.html

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",{
+    id:"parent",
+  },
+    React.createElement("div",{id:"child"},[
+    React.createElement("h1",{id:"heading1"},"I am h1 tag inside child div"),
+    React.createElement("h2",{id:"heading2"},"I am h2 tag inside child div")
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent)
