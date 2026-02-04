51f5106ddfdec4f483b9bd6298ad1c62e21c7a39
shivani3200 <shivani13222@gmail.com>
2026-02-04 14:31:40 +0530

    state change -(count button) in class component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,27 +2,49 @@ import React from "react";
 
 class UserClass extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      count:0,
-      count2:2
+      count: 0,
+      count2: 2
     }
   };
 
 
   render() {
-    const {name, email, phone} = this.props;
-    const {count, count2} = this.state;
+    const { name, email, phone } = this.props;
+    const { count, count2 } = this.state;
     return <>
       <div className="user-profile">
         <h2>User Profile</h2>
         <p>Name: {name}</p>
         <p>Ph-No: {phone}</p>
         <p>Email: {email}</p>
-        <h3>Count: {count}</h3>
+
+        <div style={{display:"flex", flexDirection:"row"}}>
+          <button onClick={() => {
+            this.setState({
+              count: this.state.count - 1
+            })
+          }}>-</button>
+
+          <div>Count: {count}</div>
+
+          <button onClick={() => {
+            // never update state variable directly -> this.state.count = this.state.count +1
+
+            this.setState({
+              count: this.state.count + 1
+            })
+          }}>+</button>
+
+        </div>
+
+
+
         <h3>Count2: {count2}</h3>
+
       </div>
     </>
   }
